d26ba48bbc528d5d135fb717a82f11d284f38f2c
Jaanika <jaanikateearu@gmail.com>
2025-05-29 10:45:05 +0300

    improved API view card layout

--- resources/js/pages/Subjects.vue
+++ resources/js/pages/Subjects.vue
@@ -13,16 +13,19 @@ defineProps(['data', 'customFields']);
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             <Card v-for="item in data" :key="item.id" class="bg-white p-4 rounded-lg shadow-md">
                 <img :src="item.image" :alt="item.title" class="w-full h-48 object-cover rounded-t-lg">
-                <h2 class="text-lg font-semibold mt-4">{{ item.title }}</h2>
-                <p class="text-lg text-gray-600 mt-2"> 
-                    {{ customFields[0] }}: {{ item[customFields[0]] }} 
-                </p>
-                <p class="text-lg text-gray-600 mt-2">
-                    {{ customFields[1] }}: {{ item[customFields[1]] }}
-                </p>
+                <h2 class="text-lg font-semibold mt-4 mb-2">{{ item.title }}</h2>
+                <div class="flex flex-wrap">
+                    <p class="text-lg font-semibold text-gray-600 mt-2"> 
+                    {{ customFields[0] }}: &nbsp </p> 
+                    <p class="text-lg text-gray-600 mt-2"> {{ item[customFields[0]] }} </p> 
+                </div>
+                <div class="flex flex-wrap">
+                    <p class="text-lg font-semibold text-gray-600 mt-2">
+                    {{ customFields[1] }}: &nbsp </p>
+                    <p class="text-lg text-gray-600 mt-2">{{ item[customFields[1]] }}
+                    </p>
+                </div>    
                 <p class="text-gray-600 mt-2">{{ item.description }}</p>
-                
-                
             </Card>
         </div>
     </AppLayout>
